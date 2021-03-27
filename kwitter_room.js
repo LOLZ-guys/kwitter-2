575ea7fd736a26dab1d57e6ac08f7c6334cf646c
LOLZ-guys <76886406+LOLZ-guys@users.noreply.github.com>
2021-03-27 09:40:25 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBu0TGXcJq98er-bH7vxtVmZKgg26LDfzw",
+    authDomain: "kwitter-873f5.firebaseapp.com",
+    databaseURL: "https://kwitter-873f5-default-rtdb.firebaseio.com",
+    projectId: "kwitter-873f5",
+    storageBucket: "kwitter-873f5.appspot.com",
+    messagingSenderId: "508475675299",
+    appId: "1:508475675299:web:6b2974778358ff96b2a516",
+    measurementId: "G-TR3RMZ1Z3D"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
